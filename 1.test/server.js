67f030e711d628127e6394ab3d9f45a0609535fd
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-10-04 20:48:46 +0300

    node intro

--- 1.test/server.js
+++ 1.test/server.js
@@ -1,5 +1,8 @@
 const os = require("os")
 const path = require("path")
+const { multiply } = require("./math")
+
+console.log(multiply(12, 30));
 
 console.log(os.hostname())
 console.log(os.platform())
