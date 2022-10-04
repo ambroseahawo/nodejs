e0998c17df727ead386dd20edc76a8033688a949
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-10-04 21:16:24 +0300

    nodejs files

--- 02Files/index.js
+++ 02Files/index.js
@@ -1,6 +1,23 @@
 const fs = require("fs")
+const path = require("path")
 
-fs.readFile("./starter.txt", "utf-8", (err, data) => {
+fs.readFile(path.join(__dirname, "files", "starter.txt"), "utf-8", (err, data) => {
   if (err) throw err;
   console.log(data)
+})
+
+fs.writeFile(path.join(__dirname, "files", "reply.txt"), "Sample write data", (err) =>{
+  if(err) throw err
+  console.log("Write completed")
+})
+
+fs.appendFile(path.join(__dirname, "files", "test.txt"), "Sample write data", (err) =>{
+  if(err) throw err
+  console.log("Append completed")
+})
+
+// exit on uncaught errors
+process.on("uncaughtException", err =>{
+  console.log(`There was an uncaught error: ${err}`)
+  process.exit
 })
