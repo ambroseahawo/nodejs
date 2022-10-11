04fea17f878b24fdde1667ba52cd7bc54db1266e
Ambrose Ahawo <ahawoadeya995@gmail.com>
2022-10-11 21:18:57 +0300

    read large files

---
+++ 02Files/stream.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+
+// readable stream
+const rs = fs.createReadStream("./files/lorem.txt",{
+  encoding: "utf-8"
+})
+
+// writable stream
+const ws = fs.createWriteStream("./files/newLorem.txt")
+
+rs.on("data", (dataChunk) =>{
+  ws.write(dataChunk)
+})
